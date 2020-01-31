386b851df037564dbd8376806e9ee5b2ae01e78f
Richard <rich.gao@hotmail.com>
2020-01-30 19:12:49 -0500

    serverhandler

---
+++ sketch_code_COMP_535/src/main/java/socs/network/node/ServerHandler.java
@@ -0,0 +1,46 @@
+package socs.network.node;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class ServerHandler implements Runnable {
+    //private RouterDescription cr;
+    private RouterDescription sr;
+    public ServerHandler(RouterDescription sr) {
+      this.sr = sr;
+    }
+  
+    public void run() {
+      System.out.println("Starting: " + sr.simulatedIPAddress.toString());
+      while(true){
+
+        try{
+          //System.out.println("HELLO");
+          ServerSocket serverSocket = new ServerSocket(sr.processPortNumber);
+          Socket server = serverSocket.accept();
+          DataInputStream in = new DataInputStream(server.getInputStream());
+          System.out.println("recieved message " + in.readUTF() + "from ???");
+          // set cr state to INIT or to TWO-WAY
+          DataOutputStream out = new DataOutputStream(server.getOutputStream());
+          out.writeUTF("HELLO");  
+
+          serverSocket.close();
+          server.close();
+
+        }catch(SocketTimeoutException s){
+          System.out.println("Socket timed out!");
+          break;
+        }catch(IOException e){
+          e.printStackTrace();
+          break;
+        }catch(Exception e){
+          e.printStackTrace();
+          break;
+        }
+      }
+    }
+  }
